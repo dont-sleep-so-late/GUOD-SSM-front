5ea2bf4a140340210925e8bcd3d9a72b87ea4ec4
dont-sleep-so-late <Imtiancai233@qq.com>
2024-04-16 20:40:01 +0800

    增加markdown编辑公告

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,66 +1,68 @@
 <template>
-    <el-row class="tac">
-        <el-col :span="12">
-            <el-menu :default-active="this.$store.state.menus.editableTabsValue" class="el-menu-vertical-demo"
-                active-text-color="#fff" :collapse="isCollapse">
-                <div class="userInfo">
-                    <div class="userHeader">
-                        <div :class="{ userAvatar: !isCollapse }">
-                            <el-avatar :size="55" shape="circle" :src="userInfo.avatar"></el-avatar>
+    <div class="Aside">
+        <el-row class="tac">
+            <el-col :span="12">
+                <el-menu :default-active="this.$store.state.menus.editableTabsValue" class="el-menu-vertical-demo"
+                    active-text-color="#fff" :collapse="isCollapse">
+                    <div class="userInfo">
+                        <div class="userHeader">
+                            <div :class="{ userAvatar: !isCollapse }">
+                                <el-avatar :size="55" shape="circle" :src="userInfo.avatar"></el-avatar>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <router-link to="/index">
-                    <el-menu-item index="index" @click="selectMenu({ name: 'index', title: '首页' })">
-                        <i class="el-icon-s-home"></i>
-                        <template slot="title">
-                            <span slot="title">首页</span>
-                        </template>
-                    </el-menu-item>
-                </router-link>
-                <router-link to="/user/equipment">
-                    <el-menu-item index="equipment" @click="selectMenu({ name: 'equipment', title: '器材列表' })">
-                        <i class="el-icon-basketball"></i>
-                        <template slot="title">
-                            <span slot="title">器材列表</span>
-                        </template>
-                    </el-menu-item>
-                </router-link>
-                <router-link to="/user/competition">
-                    <el-menu-item index="competition" @click="selectMenu({ name: 'competition', title: '竞赛列表' })">
-                        <i class="el-icon-football"></i>
-                        <template slot="title">
-                            <span slot="title">竞赛列表</span>
-                        </template>
-                    </el-menu-item>
-                </router-link>
-                <router-link to="/notice/noticeList">
-                    <el-menu-item index="noticeList" @click="selectMenu({ name: 'noticeList', title: '公告列表' })">
-                        <i class="el-icon-message-solid"></i>
-                        <template slot="title">
-                            <span slot="title">公告列表</span>
-                        </template>
-                    </el-menu-item>
-                </router-link>
-
-                <el-submenu :index="menu.name" v-for="(menu, index) in menuList" :key="index">
-                    <template slot="title">
-                        <i :class="menu.icon"></i>
-                        <span class="title">{{ menu.title }}</span>
-                    </template>
-                    <router-link :to="item.path" v-for="(item, index) in menu.children" :key="index">
-                        <el-menu-item :index="item.name" @click="selectMenu(item)">
+                    <router-link to="/index">
+                        <el-menu-item index="index" @click="selectMenu({ name: 'index', title: '首页' })">
+                            <i class="el-icon-s-home"></i>
                             <template slot="title">
-                                <i :class="item.icon"></i>
-                                <span slot="title">{{ item.title }}</span>
+                                <span slot="title">首页</span>
                             </template>
                         </el-menu-item>
                     </router-link>
-                </el-submenu>
+                    <router-link to="/user/equipment">
+                        <el-menu-item index="equipment" @click="selectMenu({ name: 'equipment', title: '器材列表' })">
+                            <i class="el-icon-basketball"></i>
+                            <template slot="title">
+                                <span slot="title">器材列表</span>
+                            </template>
+                        </el-menu-item>
+                    </router-link>
+                    <router-link to="/user/competition">
+                        <el-menu-item index="competition" @click="selectMenu({ name: 'competition', title: '竞赛列表' })">
+                            <i class="el-icon-football"></i>
+                            <template slot="title">
+                                <span slot="title">竞赛列表</span>
+                            </template>
+                        </el-menu-item>
+                    </router-link>
+                    <router-link to="/notice/noticeList">
+                        <el-menu-item index="noticeList" @click="selectMenu({ name: 'noticeList', title: '公告列表' })">
+                            <i class="el-icon-message-solid"></i>
+                            <template slot="title">
+                                <span slot="title">公告列表</span>
+                            </template>
+                        </el-menu-item>
+                    </router-link>
 
-            </el-menu>
-        </el-col>
-    </el-row>
+                    <el-submenu :index="menu.name" v-for="(menu, index) in menuList" :key="index">
+                        <template slot="title">
+                            <i :class="menu.icon"></i>
+                            <span class="title">{{ menu.title }}</span>
+                        </template>
+                        <router-link :to="item.path" v-for="(item, index) in menu.children" :key="index">
+                            <el-menu-item :index="item.name" @click="selectMenu(item)">
+                                <template slot="title">
+                                    <i :class="item.icon"></i>
+                                    <span slot="title">{{ item.title }}</span>
+                                </template>
+                            </el-menu-item>
+                        </router-link>
+                    </el-submenu>
+
+                </el-menu>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -11,11 +11,27 @@ import store from "@/store/index";
 import globalWebSocket from "./global";
 import global from "./globalFun";
 
+//导入markdown
+import VMdEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import '@kangc/v-md-editor/lib/theme/style/github.css';
+
+// highlightjs
+import hljs from 'highlight.js';
+
+VMdEditor.use(githubTheme, {
+  Hljs: hljs,
+});
+
+
 Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
 Vue.prototype.$globalWebSocket = globalWebSocket; //全局消息订阅
 
 Vue.use(ElementUI);
+
+Vue.use(VMdEditor);
 
 //创建vm
 new Vue({
